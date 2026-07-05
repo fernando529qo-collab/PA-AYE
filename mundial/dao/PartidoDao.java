f0b450b77a143eb6301155dd2e033106e679c429
José Luis <68477701+JoseLuis1705@users.noreply.github.com>
2026-07-05 02:40:36 +0000

    Se agregaron los métodos de persistencia en la clase PartidoDao

--- mundial/dao/PartidoDao.java
+++ mundial/dao/PartidoDao.java
@@ -1,53 +1,75 @@
 package mundial.dao;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.List;
-
-import mundial.bean.Bracket;
 import mundial.bean.Partido;
 import mundial.bean.RondaEliminacion;
+import mundial.bean.Bracket;
 
 public class PartidoDao {
     // CRUD estándar, más buscarPorGrupo(), buscarPorFase(), guardarResultado()
     private List<Partido> lstpartidos;
+    private Gson gson = new Gson();
+    private File file = new File("PARTIDOS.json");
 
     public PartidoDao() {
-        lstpartidos = new ArrayList<>();
+        lstpartidos = leer();
     }
 
-    public boolean crear(Partido partido) {
-        lstpartidos.add(partido);
-        return true;
+    public boolean registrar(Partido partido) throws Exception {
+        if (buscarPos(partido.getFecha()) == -1) {
+            lstpartidos.add(partido);
+            guardar(lstpartidos);
+            return true;
+        }
+        return false;
     }
 
-    public List<Partido> leer() {
+    public List<Partido> mostrar() {
         return lstpartidos;
     }
 
-    public Partido actualizar(Partido partido) {
-        int pos = 0;
-        for (Partido p : lstpartidos) {
-            if (p.getFecha().equalsIgnoreCase(partido.getFecha())) {
-                p.setEstado(partido.getEstado());
-                p.setGolesLocal(partido.getGolesLocal());
-                p.setGolesVisitante(partido.getGolesVisitante());
-                p.setPartidoEstadisticas(partido.getPartidoEstadisticas());
-                p.setProbabilidadVictoriaLocal(partido.getProbabilidadVictoriaLocal());
-                p.setProbabilidadVictoriaVisitante(partido.getProbabilidadVictoriaVisitante());
-                lstpartidos.set(pos, partido);
+    public int buscarPos(String fecha) {
+        for (int i = 0; i < lstpartidos.size(); i++) {
+            if (lstpartidos.get(i).getFecha().equalsIgnoreCase(fecha)) {
+                return i;
             }
-            pos++;
         }
-        return lstpartidos.get(pos);
+        return -1;
     }
 
-    public boolean eliminar() {
-        if (!lstpartidos.isEmpty()) {
-            lstpartidos.removeAll(lstpartidos);
-            return true;
-        } else {
-            return false;
+    public Partido buscarObj(String fecha) {
+        for (int i = 0; i < lstpartidos.size(); i++) {
+            if (lstpartidos.get(i).getFecha().equalsIgnoreCase(fecha)) {
+                return lstpartidos.get(i);
+            }
         }
+        return null;
+    }
+
+    public boolean actualizar(Partido partido) throws Exception {
+        if (buscarPos(partido.getFecha()) != -1) {
+            lstpartidos.set(buscarPos(partido.getFecha()), partido);
+            guardar(lstpartidos);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean eliminar(Partido partido) throws Exception {
+        int pos = buscarPos(partido.getFecha());
+        if (pos != -1) {
+            lstpartidos.remove(pos);
+            guardar(lstpartidos);
+            return true;
+        }
+        return false;
     }
 
     public List<Partido> buscarPorGrupo(char codigo) {
@@ -75,5 +97,32 @@ public class PartidoDao {
             pos++;
         }
         return lstPartidosEscontrados;
+    }
+
+    public List<Partido> leer() {
+        try {
+            List<Partido> partidos;
+            if (!file.exists()) {
+                return new ArrayList<>();
+            }
+            try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+                partidos = gson.fromJson(br.readLine(), new TypeToken<ArrayList<Partido>>() {
+                }.getType());
+            }
+            if (partidos == null) {
+                partidos = new ArrayList<>();
+            }
+            return partidos;
+        } catch (Exception e) {
+            return new ArrayList<>();
+        }
+    }
+    
+    public void guardar(List<Partido> lista) throws Exception {
+        try (FileWriter fw = new FileWriter(file)) {
+            gson.toJson(lista, fw);
+        } catch (Exception e) {
+            throw e;
+        }
     }
 }
