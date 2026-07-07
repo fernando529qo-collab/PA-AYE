3a65c074d7f3abcfbda7470d3fd9df1adb7dd1c4
José Luis <68477701+JoseLuis1705@users.noreply.github.com>
2026-07-07 13:40:45 +0000

    Se crearon y completaron las clases TablaPosicionDao y GrupoDao

--- mundial/dao/GrupoDao.java
+++ mundial/dao/GrupoDao.java
@@ -1,5 +1,47 @@
 package mundial.dao;
 
-public class GrupoDao {
+import java.util.ArrayList;
 
+import mundial.bean.Grupo;
+
+public class GrupoDao {
+    private static ArrayList<Grupo> listaGrupos = new ArrayList<>();
+
+    public static boolean guardarGrupo(Grupo grupo) {
+        return listaGrupos.add(grupo);
+    }
+
+    public static Grupo buscarPorIdGrupo(char codigo) {
+
+        for (Grupo grupo : listaGrupos) {
+            if (grupo.getCodigo() == codigo) {
+                return grupo;
+            }
+        }
+        return null;
+    }
+
+    public static ArrayList<Grupo> listarTodos() {
+        return listaGrupos;
+    }
+
+    public static boolean actualizarGrupo(Grupo grupoActualizado) {
+        for (int i = 0; i < listaGrupos.size(); i++) {
+            if (listaGrupos.get(i).getCodigo() == grupoActualizado.getCodigo()) {
+                listaGrupos.set(i, grupoActualizado);
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+    public static boolean eliminarGrupo(char codigo) {
+        Grupo grupo = buscarPorIdGrupo(codigo);
+        if (grupo != null) {
+            listaGrupos.remove(grupo);
+            return true;
+        }
+        return false;
+    }
 }
--- mundial/dao/TablaPosicionDao.java
+++ mundial/dao/TablaPosicionDao.java
@@ -1,5 +1,46 @@
 package mundial.dao;
 
-public class TablaPosicionDao {
+import java.util.ArrayList;
 
+import mundial.bean.Pais;
+import mundial.bean.TablaPosicion;
+
+public class TablaPosicionDao {
+    private static ArrayList<TablaPosicion> listaTabla = new ArrayList<>();
+    
+    public static boolean guardarTabla(TablaPosicion tabla) {
+        return listaTabla.add(tabla);
+    }
+
+    public static TablaPosicion buscarPorIdTabla(Pais pais) {
+        for (TablaPosicion tabla : listaTabla) {
+            if (tabla.getPais().equals(pais)) {
+                return tabla;
+            }
+        }
+        return null;
+    }
+
+    public static ArrayList<TablaPosicion> listarTodas() {
+        return listaTabla;
+    }
+
+    public static boolean actualizarTabla(TablaPosicion tablaActualizada) {
+        for (int i = 0; i < listaTabla.size(); i++) {
+            if (listaTabla.get(i).getPais().equals(tablaActualizada.getPais())) {
+                listaTabla.set(i, tablaActualizada);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean eliminarTabla(Pais pais) {
+        TablaPosicion tabla = buscarPorIdTabla(pais);
+        if (tabla != null) {
+            listaTabla.remove(tabla);
+            return true;
+        }
+        return false;
+    }
 }
