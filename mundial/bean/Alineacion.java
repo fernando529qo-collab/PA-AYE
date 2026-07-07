b537c16e84702c951442532d3d54d6dff10b025b
José Luis <68477701+JoseLuis1705@users.noreply.github.com>
2026-07-07 23:35:57 +0000

    Se cambio la List por Map en la clase Alineacion

--- mundial/bean/Alineacion.java
+++ mundial/bean/Alineacion.java
@@ -4,7 +4,9 @@
  */
 package mundial.bean;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -13,15 +15,18 @@ import java.util.List;
 public class Alineacion {
     private Partido partido;
     private Pais pais;
-    private List<Jugador> jugadoresTitulares;
+    // private List<Jugador> jugadoresTitulares;
+    // Se reemplaza por la List por qué el Map ayuda con la asociación de Jugador y Posicion, clave para el manejo del logic
+    // Al usar Map sirve para logar el manejo de la vista dinamica e interacctiva al momento de escoger los jugadores (plantilla) en la simulacion
+    private Map<Jugador, Posicion> jugadoresTitulares;
 
     public Alineacion() {
     }
 
-    public Alineacion(Partido partido,Pais pais, List<Jugador> jugadoresTitulares) {
+    public Alineacion(Partido partido,Pais pais) {
         this.partido = partido;
         this.pais = pais;
-        this.jugadoresTitulares = jugadoresTitulares;
+        this.jugadoresTitulares = new HashMap<>();
     }
 
     public Partido getPartido() {
@@ -40,12 +45,16 @@ public class Alineacion {
         this.pais = pais;
     }
 
-    public List<Jugador> getJugadoresTitulares() {
+    public Map<Jugador, Posicion> getJugadoresTitulares() {
         return jugadoresTitulares;
     }
 
-    public void setJugadoresTitulares(List<Jugador> jugadoresTitulares) {
+    public void setJugadoresTitulares(Map<Jugador, Posicion> jugadoresTitulares) {
         this.jugadoresTitulares = jugadoresTitulares;
+    }
+
+    public void agregarJugador(Jugador jugador, Posicion posicionSeleccionada) {
+        this.jugadoresTitulares.put(jugador, posicionSeleccionada);
     }
 
     @Override
